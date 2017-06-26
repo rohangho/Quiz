495b367d508c1ee0cfec8293ea2fb7a8db59d6cf
Rohan <rohangho@live.com>
2017-06-26 19:53:10 +0530

    indentation

--- QuizMaster/app/src/main/java/com/example/android/quizmaster/MainActivity.java
+++ QuizMaster/app/src/main/java/com/example/android/quizmaster/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
 import com.example.android.quizmaster.R;
 
 import static android.R.attr.name;
@@ -22,59 +23,73 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    int m=0;
-    public void SUBMIT (View view){
-        int a=0;
 
+    int m = 0;
 
-        EditText name=(EditText) findViewById(R.id.NAME);;
-        String name1= name.getText().toString();
+    public void SUBMIT(View view) {
+        int a = 0;
 
-        EditText animal=(EditText) findViewById(R.id.ANIMAl);;
-        String animal1= animal.getText().toString();
+        EditText name = (EditText) findViewById(R.id.NAME);
+        ;
+        String name1 = name.getText().toString();
 
-        CheckBox milk=(CheckBox) findViewById(R.id.MILK);
-        boolean milk1=milk.isChecked();
+        EditText animal = (EditText) findViewById(R.id.ANIMAl);
+        ;
+        String animal1 = animal.getText().toString();
 
-        CheckBox icecream=(CheckBox) findViewById(R.id.ICECREAM);
-        boolean icecream1=icecream.isChecked();
+        CheckBox milk = (CheckBox) findViewById(R.id.MILK);
+        boolean milk1 = milk.isChecked();
 
-        boolean checked=((RadioButton) view).isChecked();
+        CheckBox icecream = (CheckBox) findViewById(R.id.ICECREAM);
+        boolean icecream1 = icecream.isChecked();
 
-        switch(view.getId()){
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
             case R.id.TEN:
-                if(checked){a=a+1;}
+                if (checked) {
+                    a = a + 1;
+                }
+                break;
+            case R.id.TWENTY:
+                if (checked) {
+                    a = 0;
+                }
+                break;
+            case R.id.THIRTY:
+                if (checked) {
+                    a = 0;
+                }
                 break;
 
         }
 
-
-        int c= scorecalculator(milk1,icecream1,a,animal1,name1);
-
+        int c = scorecalculator(milk1, icecream1, a, animal1, name1);
         display(c);
-
-
     }
-
-    public   int scorecalculator (boolean a, boolean b, int f,String d,String e){
-        int m=0;
-        if (a==true&&b==true)
-            m=m+1;
-        if(f==1){m=m+1;}
-
-        if (d=="India"){m=m+1;}
-        if (e=="Bill Gates")
-            m=m+1;
+    public int scorecalculator(boolean a, boolean b, int f, String d, String e) {
+        int m = 0;
+        if (a == true && b == true)
+            m = m + 1;
+        if (f == 1) {
+            m = m + 1;
+        }
+        if (d == "India") {
+            m = m + 1;
+        }
+        if (e == "Bill Gates")
+            m = m + 1;
 
         return m;
     }
-    public void RESET(View view){
-        int m=0;
+
+    public void RESET(View view) {
+        int m = 0;
         display(m);
     }
 
-    public void display(int a){
-        TextView textView = (TextView)findViewById(R.id.SCORE);
+    public void display(int a) {
+        TextView textView = (TextView) findViewById(R.id.SCORE);
         textView.setText(a);
 
     }
